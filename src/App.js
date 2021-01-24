6beb358121c27e6989152bab5b8580f5f666149f
Junaid S <intelpik@gmail.com>
2021-01-24 19:42:44 +0530

    started adding styles to login page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,6 @@ import Login from './Login';
 function App() {
   return (
     <div className="App">
-      <img
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-        alt=""
-      />
-
-     {/* Login with spotify button */}
      <Login/>
     </div>
   );
