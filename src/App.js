4b0d7108317069a27fbce5579affbbe18b1bf42a
Junaid S <intelpik@gmail.com>
2021-01-24 18:32:48 +0530

    creating login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-     
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+
+     {/* Login with spotify button */}
+     <Login/>
     </div>
   );
 }
