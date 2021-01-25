59f42a8e1dffb5237753d00a502d7af3f228d60e
Junaid S <intelpik@gmail.com>
2021-01-25 07:02:10 +0530

    added styling to Player.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 import { useStateValue } from "./StateProvider";
 import Player from "./Player";
